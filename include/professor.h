73f117182116a642a31f0a4be5bb11917d8bcd13
yerzakovakmal <akmal63337@gmail.com>
2026-04-10 22:17:01 +0500

    Add functionalities of professor only for header file

--- include/professor.h
+++ include/professor.h
@@ -1,25 +1,39 @@
+#include "user.h"
+#include "course.h"
 #include <iostream>
 #include <string>
-#include "user.h"
+#include <vector>
+#include <stdexcept>
 using namespace std;
 
-class Professor : public User{
-    private:
+class Professor : virtual public User{
+    private: 
+        string employeeID;
         string department;
-        string subject;
+        vector<Course*> taughtCourses;
     public:
-        Professor(int id, int age, string f_name, string l_name, string department, string subject) : User(id, age, f_name, l_name){
-            setProfessor(department, subject);
-        }
 
-        void setProfessor(string dep, string sub){
-            department = dep;
-            subject = sub;
-        }
+        Professor(string name, string email, string password, string empId, string dept);
+        ~Professor() override;
+        
+        //getters
+        string getEmployeeId() const;
+        string getDepartment() const;
+        vector<Course*> getTaughtCourses() const;
+        
 
-        void displayProfessor() const{
-            displayUser();
-            cout << "Department: " << department << endl;
-            cout << "Subject: " << subject << endl;
-        }
+        // setters
+        void createCourse(Course* course);
+        void assignGrade(Student& student, Course& course, double score);
+        void postMaterial(string material, Course& course);
+        void createAssignment(Course& course, string title, string description, double maxScore);
+        string generateReport(Course& course) const;
+        void assignTA(TeachingAssistant& ta, Course& course);
+
+        //operator overloads
+        bool operator==(const Professor& other) const;
+        friend ostream& operator<<(ostream& os, const Professor& p);
+
+        string getRole() const override;
+        void displayInfo() const override;
 };
