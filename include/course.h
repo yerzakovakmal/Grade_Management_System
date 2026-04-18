497272b882033512c9706e24b051cb6187bc5aec
yerzakovakmal <akmal63337@gmail.com>
2026-04-18 12:12:21 +0500

    Added all required functionalities of Course, next the implementation in cpp file

--- include/course.h
+++ include/course.h
@@ -2,10 +2,61 @@
 #include <string>
 using namespace std;
 
+class Assignment;
 class Course{
     private:
-        int C_ID;
-        string course_name;
+        string courseID;
+        string courseName;
+        int credits;
+        int capacity;
+        vector<Student*> enrolledStudents;
+        vector<Assignment> assignments;
+        vector<string> materials;
+        vector<string> announcements;
+        Professor* professor;
+        static int CourseCount;
     public:
-        //something
+        Course(string CourseID, string CourseName, int credits, int capacity);
+        ~Course();
+
+        //getters
+        string getCourseID() const;
+        string getCourseName() const;
+        int getCredits() const;
+        int getCapacity() const;
+        int EnrollmentCount() const;
+        vector<Student*> getEnrolledStudents() const;
+        vector<Assignment*> getAssignments();
+        const vector<Assignment*> getAssignments() const;
+        vector<string> getMaterials() const;
+        vector<string> getAnnouncements() const;
+        Professor* getProfessor() const;
+        static int getTotalCourses();
+
+        //setters
+        void setTitle(string title);
+        void setDueDate(string date);
+        void setMaxscore(double score);
+
+        //student management
+        void addStudent(Student* student);
+        void removeStudent(Student* student);
+        bool isEnrolled(Student* student) const;
+        bool isFull() const;
+
+        //content management
+        void addAssignment(const Assignment& a);
+        void addMaterial(string material);
+        void addAnnouncement(string msg);
+
+        //file input/outpu
+        void saveToFile(string filename) const;
+        static Course loadFromFile(string filename);
+
+        //operator overload
+        friend ostream& operator<<(ostream& out, const Course& c);
+        bool operator==(const Course& other) const;
+        bool operator!=(const Course& other) const;
+        bool operator<(const Course& other) const;
+        bool operator>(const Course& other) const;
 };
